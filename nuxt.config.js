1de11d6f7c62bf66f14e0f071d32fe0d8abcede1
makowis <mako.mail.g@gmail.com>
2021-01-22 17:43:51 +0900

    lint fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/nuxttest/'
+    base: '/nuxttest/',
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -20,7 +20,9 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/nuxttest/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/nuxttest/favicon.ico' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
