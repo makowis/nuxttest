4b75b320854bf1e351900ebdf810fcb40c1f0dfe
makowis <mako.mail.g@gmail.com>
2021-01-22 17:36:39 +0900

    add deploy github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/nuxttest/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxttest',
@@ -16,7 +20,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/nuxttest/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
