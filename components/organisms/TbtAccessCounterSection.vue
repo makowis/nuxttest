112d891b6591b382c31b0fa2705e4d3c7eebd344
makowis <mako.mail.g@gmail.com>
2021-01-22 19:27:32 +0900

    delete link bbs

--- components/organisms/TbtAccessCounterSection.vue
+++ components/organisms/TbtAccessCounterSection.vue
@@ -3,9 +3,6 @@
     <tbt-h-2>アクセスカウンター</tbt-h-2>
     <p>あなたは{{ access }}人目の訪問者です。</p>
     <p>現在の累計訪問者数は{{ realtime }}人です。</p>
-    <small>
-      <router-link to="/bbs">キリ番報告はこちらで！！！</router-link>
-    </small>
   </tbt-section>
 </template>
 
